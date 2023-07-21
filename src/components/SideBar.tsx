e4faa733f9acc55ae5056c2d0a8c9977426ee7aa
songen01 <songen@storswift.com>
2023-07-21 16:15:11 +0800

    chat add some code

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -12,7 +12,7 @@ export function SideBar() {
       <div className="flex flex-col w-20vw h-screen px-4 py-8 border-x-1 bg-grey dark:bg-black justify-between items-center">
         <div className="flex flex-col w-44 h-screen  border-gray-200 bg-grey dark:bg-black justify-start items-center">
           <Link href="/"><Image src="/logo.png" width={50} height={50} alt="Logo" /></Link>
-          <Link href="/chat"><Image className='my-8' src="/chat.svg" width={30} height={30} alt="Logo" /></Link>
+          <Link href="/chat/general"><Image className='my-8' src="/chat.svg" width={30} height={30} alt="Logo" /></Link>
           <Link href="/moment"><Image src="/moment.svg" width={30} height={30} alt="Logo" /></Link>
         </div>
         <div className="flex flex-col items-center mt-6 -mx-2 text-red">
