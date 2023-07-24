9d7a8a6c264b473897fa96ce43941fa40dd6418e
songen01 <songen@storswift.com>
2023-07-24 18:37:01 +0800

    feat: fill content data

--- src/components/pages/Chat/ChatInput.tsx
+++ src/components/pages/Chat/ChatInput.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react'
 import { useTheme } from 'next-themes'
 import { CloudIcon, EmojiIcon, LockIcon, PictureIcon, SendIcon, SpeechIcon } from './Icons'
-import { FillColor } from '@/type/SvgIcon'
+import { FillColor } from '@/type/Chat'
 
 export function ChatInput() {
   const inputIcon = ['emojy', 'picture', 'lock', 'cloud', 'speech']
@@ -18,24 +18,24 @@ export function ChatInput() {
   }
   const handleFillColor = (): FillColor => theme === 'dark' ? FillColor.White : FillColor.Black
   return <div className='p-4  w-full  h-[130px] '>
-        <div className='bg-neutral-200/25 dark:bg-neutral-500/25 h-full rounded-xl flex justify-between flex-col p-2'>
-            <input
-                className="w-full mx-4 h-full bg-transparent outline-none"
-                onKeyDown={handleKeyDown}
-                onChange={(e) => { setInputData(e.target.value) }}
-                type="text"
-            />
-            <div className=' border-b m-2 border-neutral-200 dark:border-neutral-700' />
-            <div className="h-full mx-3 flex items-center justify-between">
-                <div className="h-full  flex items-center gap-4">
-                    <EmojiIcon fill={handleFillColor()}></EmojiIcon>
-                    <LockIcon fill={handleFillColor()}></LockIcon>
-                    <PictureIcon fill={handleFillColor()}></PictureIcon>
-                    <SpeechIcon fill={handleFillColor()}></SpeechIcon>
-                    <CloudIcon fill={handleFillColor()}></CloudIcon>
-                </div>
-                <SendIcon fill={handleFillColor()} />
-            </div>
+    <div className='bg-neutral-200/25 dark:bg-neutral-500/25 h-full rounded-xl flex justify-between flex-col p-2'>
+      <input
+        className="w-full mx-4 h-full bg-transparent outline-none"
+        onKeyDown={handleKeyDown}
+        onChange={(e) => { setInputData(e.target.value) }}
+        type="text"
+      />
+      <div className=' border-b m-2 border-neutral-200 dark:border-neutral-700' />
+      <div className="h-full mx-3 flex items-center justify-between">
+        <div className="h-full  flex items-center gap-4">
+          <EmojiIcon fill={handleFillColor()}></EmojiIcon>
+          <LockIcon fill={handleFillColor()}></LockIcon>
+          <PictureIcon fill={handleFillColor()}></PictureIcon>
+          <SpeechIcon fill={handleFillColor()}></SpeechIcon>
+          <CloudIcon fill={handleFillColor()}></CloudIcon>
         </div>
+        <SendIcon fill={handleFillColor()} />
+      </div>
     </div>
+  </div>
 }
