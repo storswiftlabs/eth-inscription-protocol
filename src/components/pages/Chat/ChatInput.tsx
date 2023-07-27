0d167f830f0cc15c4835c53fdf46f1b6548c7257
songen01 <songen@storswift.com>
2023-07-27 10:40:58 +0800

    add send message with loading

--- src/components/pages/Chat/ChatInput.tsx
+++ src/components/pages/Chat/ChatInput.tsx
@@ -3,13 +3,13 @@
 import { useEffect, useState } from 'react'
 import { useTheme } from 'next-themes'
 import { useSendTransaction, useWalletClient } from 'wagmi'
+import { Loading } from '@nextui-org/react'
 
 import { CloudIcon, EmojiIcon, LockIcon, PictureIcon, SendIcon, SpeechIcon } from './Icons'
 import { FillColor } from '@/type/Chat'
 import '@/style/chat/ChatInput.css'
 
 export function ChatInput() {
-  const inputIcon = ['emojy', 'picture', 'lock', 'cloud', 'speech']
   const { theme } = useTheme()
   const [inputData, setInputData] = useState('')
   const [themeColor, setThemeColor] = useState('')
@@ -21,8 +21,10 @@ export function ChatInput() {
   const handleKeyDown = (e: any) => {
     if (e.key === 'Enter')
       sendTransaction()
-    isSuccess && setInputData('')
   }
+  useEffect(() => {
+    isSuccess && setInputData('')
+  }, [isSuccess])
 
   const handleSend = () => {
     sendTransaction()
@@ -35,13 +37,15 @@ export function ChatInput() {
 
   return <div className='p-4  w-full  h-[130px] '>
     <div className='chat-input h-full rounded-xl flex justify-between flex-col p-2'>
-      <input
-        className="w-full mx-4 h-full bg-transparent outline-none"
-        onKeyDown={handleKeyDown}
-        value={inputData}
-        onChange={(e) => { setInputData(e.target.value) }}
-        type="text"
-      />
+      {isLoading
+        ? <div className='h-full w-full mx-4'> <Loading /></div>
+        : <input
+          className="w-full mx-4 h-full bg-transparent outline-none"
+          onKeyDown={handleKeyDown}
+          value={inputData}
+          onChange={(e) => { setInputData(e.target.value) }}
+          type="text"
+        />}
       <div className=' border-b m-2 division-line' />
       <div className="h-full mx-3 flex items-center justify-between">
         <div className="h-full  flex items-center gap-4">
