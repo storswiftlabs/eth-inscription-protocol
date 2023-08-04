44b16e3c19faf44a27d1e701182419caa883cc51
songen <614750803@qq.com>
2023-08-04 11:01:48 +0800

    add cors

--- backend/internal/server/http.go
+++ backend/internal/server/http.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/gorilla/handlers"
 )
 
 // NewHTTPServer new an HTTP server.
@@ -16,6 +17,10 @@ func NewHTTPServer(c *conf.Server, greeter *service.InscriptionService, logger l
 		http.Middleware(
 			recovery.Recovery(),
 		),
+		http.Filter(handlers.CORS(
+			handlers.AllowedOrigins([]string{"*"}),
+			handlers.AllowedMethods([]string{"GET", "POST"}),
+		)),
 	}
 	if c.Http.Network != "" {
 		opts = append(opts, http.Network(c.Http.Network))
