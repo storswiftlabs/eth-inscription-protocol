023ca8e02df91472a5335306246a6ebb0aa6fa7d
qiancheng12 <wancheng@storswift.com>
2023-08-07 16:43:30 +0800

    feat:tweet动态 联调

--- src/utils/imageFormat.ts
+++ src/utils/imageFormat.ts
@@ -4,8 +4,6 @@ export const imageFormat = (imgSrc: string) => {
   if (imgSrc&&imgSrc.indexOf("https://") > -1) {
     return imgSrc;
   } else {
-    console.log( `https://ipfs.io/ipfs/${imgSrc?.substring(7).split("?")[0]}`);
-    
     return `https://ipfs.io/ipfs/${imgSrc?.substring(7).split("?")[0]}`;
   }
 };
