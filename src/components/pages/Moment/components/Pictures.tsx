ae895d97fb27d4ae5f4676228b31293a6a953edf
qiancheng12 <wancheng@storswift.com>
2023-08-23 11:42:43 +0800

    file img

--- src/components/pages/Moment/components/Pictures.tsx
+++ src/components/pages/Moment/components/Pictures.tsx
@@ -13,17 +13,20 @@ function Pictures({ pictureArr, setPictureArr }: Props) {
   const fileRef = useRef<HTMLInputElement>(null)
 
   const handleFillColor = (): FillColor => theme === 'dark' ? FillColor.White : FillColor.Black
-
+  console.log(pictureArr,'pictureArr');
+  
   return (
     <>
       <input type="file" ref={fileRef} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target?.files && e.target?.files[0]
+        file && setPictureArr([...pictureArr, file])
         const reader = new FileReader()
         reader.onload = function (e) {
           const dataURL = reader.result as string
-          setPictureArr([...pictureArr, dataURL])
+          // setPictureArr([...pictureArr, file])
         }
         file && reader.readAsDataURL(file)
+        
       }} className='w-0 h-0 none' />
       <PictureIcon onClick={() => fileRef.current?.click()} fill={handleFillColor()}></PictureIcon>
     </>
